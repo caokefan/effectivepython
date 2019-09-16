fe1721f0690b9fe02afc33da1fad091c3064faa5
2leimeng <ixlm37@163.com>
2019-09-16 16:04:25 +0800

    sixth commit

--- item_30.py
+++ item_30.py
@@ -1,6 +1,6 @@
 from datetime import timedelta, datetime
 
-
+'''
 class Bucket(object):
     def  __init__(self, period):
         self.period_delta = timedelta(seconds=period)
@@ -9,7 +9,7 @@ class Bucket(object):
 
     def __repr__(self):
         return 'Bucket（quota=%d)' % self.quota
-
+'''
 def fill(bucket, amount):
     now = datetime.now()
     if now - bucket.reset_time > bucket.period_delta:
@@ -25,7 +25,7 @@ def deduct(bucket, amount):
         return False
     bucket.quota -= amount
     return True
-
+'''
 bucket = Bucket(60)
 fill(bucket, 100)
 print(bucket)
@@ -35,3 +35,49 @@ if deduct(bucket, 99):
 else:
     print('Not enough for 99 quota')
 print(bucket)
+'''
+class Bucket(object):
+    def __init__(self, period):
+        self.period_delta = timedelta(seconds=period)
+        self.reset_time = datetime.now()
+        self.max_quota = 0
+        self.quota_consumed = 0
+
+    def __repr__(self):
+        return ('Bucket(max_quota=%d, quota_consumed=%d') % (self.max_quota, self.quota_consumed)
+
+    @property
+    def quota(self):
+        return self.max_quota - self.quota_consumed
+
+    @quota.setter
+    def quota(self, amount):
+        delta = self.max_quota - amount
+        if amount == 0:
+            self.quota_consumed = 0
+            self.max_quota = 0
+        elif delta < 0:
+            assert self.quota_consumed == 0
+            self.max_quota = amount
+        else:
+            assert self.max_quota >= self.quota_consumed
+            self.quota_consumed += delta
+
+bucket = Bucket(60)
+print('Initial', bucket)
+fill(bucket, 100)
+print('Filled', bucket)
+
+if deduct(bucket, 99):
+    print('Had 99 quota')
+else:
+    print('Not enough for 99 quota')
+
+print('Now', bucket)
+
+if deduct(bucket, 3):
+    print('Had 3 quota')
+else:
+    print('Not enough for 3 quota')
+
+print('Still', bucket)
