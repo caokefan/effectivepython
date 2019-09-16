31f850f16f20931697a2c0bf3e9fe0b3fdd6eae5
2leimeng <ixlm37@163.com>
2019-09-16 16:52:58 +0800

    sixth commit

--- item_16.py
+++ item_16.py
@@ -1,5 +1,10 @@
 # consider using a generator to override a function that returns a list directly
-address = 'Four score and seven yesrs ago...'
+# using generator is more clear than returning result as a list to the caller
+# no matter how big the input is, the generator can generate a series of outputs
+# because these inputs and outputs will not affect the memory consumed during execution
+from itertools import islice
+
+address = 'Four score and seven years ago...'
 '''
 def index_words(text):
     result = []
@@ -23,3 +28,18 @@ def index_words_iter(text):
 
 result = list(index_words_iter(address))
 print(result)
+
+def index_file(handle):
+    offset = 0
+    for line in handle:
+        if line:
+            yield offset
+            for letter in line:
+                offset += 1
+                if letter == ' ':
+                    yield offset
+
+with open('item_16_address.txt', 'r') as f:
+    it = index_file(f)
+    results = islice(it, 0, 3)
+    print(list(results))
--- item_17.py
+++ item_17.py
@@ -1,5 +1,10 @@
-# be careful when iterating over the parameters
-visits = [15, 35, 80]
+# be careful when iterating over the parameters: if the argument is an iterator , it
+# can cause strange behavior and miss some values.
+# python iterator protocol which describes how containers and iterators should
+# interact with iter() and next() built-in functions, 'for' loops and related expressions.
+# implement __iter__() as a generator to define your own container type.
+# to judge whether a value is an iterator or a container, call the iter() twice
+# while the result is same iterator, call next() to make the iterator go one step further.
 
 def normalize(numbers):
     total = sum(numbers)
@@ -9,6 +14,7 @@ def normalize(numbers):
         result.append(percent)
     return result
 '''
+visits = [15, 35, 80]
 percentages = normalize(visits)
 print(percentages)
 '''
@@ -27,7 +33,7 @@ def normalize_copy(numbers):
         result.append(percent)
     return result
 
-it = read_visits('my_numbers.txt')
+it = read_visits('item_17_my_numbers.txt')
 
 '''
 print(list(it))
@@ -37,7 +43,50 @@ print(list(it))
 '''
 percentages = normalize(it)
 print(percentages)
-'''
+
 
 percentages = normalize_copy(it)
 print(percentages)
+'''
+def normalize_func(get_iter):
+    total = sum(get_iter())
+    result = []
+    for value in get_iter():
+        percent = 100 * value / total
+        result.append(percent)
+    return result
+
+path = 'item_17_my_numbers.txt'
+percentages = normalize_func(lambda: read_visits(path))
+print(percentages)
+
+class ReadVisits(object):
+    def __init__(self, data_path):
+        self.data_path = data_path
+
+    def __iter__(self):
+        with open(self.data_path) as f:
+            for line in f:
+                yield int(line)
+
+visits = ReadVisits(path)
+percentages = normalize(visits)
+print(percentages)
+
+def normalize_defensive(numbers):
+    if iter(numbers) is iter(numbers):
+        raise TypeError('Must supply a container')
+    total = sum(numbers)
+    result = []
+    for value in numbers:
+        percent = 100 * value / total
+        result.append(percent)
+    return result
+
+visits = [15, 35, 80]
+normalize_defensive(visits)
+visits = ReadVisits(path)
+normalize_defensive(visits)
+
+it = iter(visits)
+normalize_defensive(it)
